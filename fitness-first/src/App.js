819e3ff88c2329d07e3a1707051c77b7a7dccd2f
Ruchi <ruchiagrawal9186@gmail.com>
2023-05-04 00:10:53 -0800

    day-1 slider and artical component done.

--- fitness-first/src/App.js
+++ fitness-first/src/App.js
@@ -4,6 +4,8 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import { Link } from 'react-router-dom';
 
+
+
 function App() {
   return (
     <div className="App">
